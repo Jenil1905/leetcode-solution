ef32f106b6280014effc440d01f0a01860ded87a
Jenil <langaliyajenil@gmail.com>
2026-08-23 18:11:00 +0530

    Time: 31 ms (82.26%), Space: 60.4 MB (9.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+         List<List<Integer>> ans = new ArrayList<>();
+         for(int i=0; i<nums.length-2; i++){
+
+            //skip the duplicate
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+
+            int left = i+1;
+            int right = nums.length-1;
             int target = -nums[i];
-            int j = i+1;
-            int k = nums.length-1;
-            while(j<k){
-                int sum = nums[j]+nums[k];
+            while(left<right){
+                int sum = nums[left]+nums[right];
                 if(sum==target){
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    list.add(nums[i]);
-                    ans.add(list);
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) { j++; }
-    while (j < k && nums[k] == nums[k + 1]) {
-        k--;
-    }
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[left]);
+                    temp.add(nums[i]);
+                    temp.add(nums[right]);
+                    ans.add(temp);
+                    left++;
+                    right--;
+                    //skip duplicate left values
+                    while(left<right && nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    //skip duplicate right values
+                    while(left<right && nums[right]==nums[right+1]){
+                        right--;
+                    }
                 }else if(sum>target){
-                    k--;
+                    right--;
                 }else{
-                    j++;
+                    left++;
                 }
             }
-        }
-        return ans;
+         }
+         return ans;
     }
 }
